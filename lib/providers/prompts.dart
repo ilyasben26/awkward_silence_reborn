018cb253ecd8cfeff4fe9e23674593227323904d
IlyasBen <73348981+ilyasben26@users.noreply.github.com>
2023-03-03 12:09:14 +0100

    modified prompts.dart with new format

--- lib/providers/prompts.dart
+++ lib/providers/prompts.dart
@@ -8,29 +8,35 @@ class Prompt {
 }
 
 class Prompts with ChangeNotifier {
-  final List<String> _prompts = [
-    "What's something that you've always wanted to try but haven't had the chance to yet?",
-    "If you could have a conversation with your past self, what advice would you give?",
-    "Hey, I couldn't help but notice your [accessory or clothing item]. It looks really cool. Where did you get it?",
-    "Hey, I'm on a mission to meet the most interesting people in this room, and I think I just found one. Can I buy you a drink and hear your story?"
+  final List<Prompt> _prompts = [
+    Prompt(
+      text:
+          "What's something that you've always wanted to try but haven't had the chance to yet?",
+      category: "question",
+    ),
+    Prompt(
+      text:
+          "If you could have a conversation with your past self, what advice would you give?",
+      category: "question",
+    ),
   ];
 
-  String _randPrompt = '';
+  Prompt _randPrompt = Prompt(text: "", category: 'question');
 
-  List<String> get prompts {
+  List<Prompt> get prompts {
     return [..._prompts];
   }
 
-  String get randPrompt {
+  Prompt get randPrompt {
     return _randPrompt;
   }
 
   void setRandPrompt() {
-    String oldPrompt = _randPrompt;
+    Prompt oldPrompt = _randPrompt;
 
-    String newPrompt = _prompts[Random().nextInt(_prompts.length)];
+    Prompt newPrompt = _prompts[Random().nextInt(_prompts.length)];
 
-    while (oldPrompt == newPrompt) {
+    while (oldPrompt.text == newPrompt.text) {
       newPrompt = _prompts[Random().nextInt(_prompts.length)];
     }
 
